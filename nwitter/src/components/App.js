43fabe169e87e9b5b426d743f6e46bc1f7778ea6
kkamang <joywithjoy1203@gamil.com>
2023-11-13 00:41:19 +0900

    Install Styled-Components

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import AppRouter from "./Router";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      
+    </>
+  );
 }
 
 export default App;
